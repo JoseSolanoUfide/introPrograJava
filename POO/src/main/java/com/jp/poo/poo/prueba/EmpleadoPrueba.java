4b6209124395950b3a43f5813aeafe7115ceea91
jpsolano <jpsolano@jpsolano-HP-ProDesk-600-G4-DM-TAA>
2024-10-19 09:19:11 -0600

    update

---
+++ POO/src/main/java/com/jp/poo/poo/prueba/EmpleadoPrueba.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jp.poo.poo.prueba;
+
+import com.jp.poo.poo.empleado.Empleado;
+
+/**
+ *
+ * @author jpsolano
+ */
+public class EmpleadoPrueba {
+    public static void main(String[] args) {
+        // Crear empleados
+        Empleado empleado1 = new Empleado();
+        empleado1.salarioBase = 100.0;
+        empleado1.edad = 30;
+        empleado1.departamento = "Contabilidad";
+        empleado1.teletrabajo = true;
+        empleado1.calcularSueldo(empleado1);
+
+    }
+}
